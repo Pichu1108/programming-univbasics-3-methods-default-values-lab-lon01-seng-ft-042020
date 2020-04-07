0c93852225ec8845eaa1fb6deeb66713d0a92ec4
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-04-07 01:54:33 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,8 +1,4 @@
 def meal_choice (veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
   return "A plate of #{protein} with #{veg1} and #{veg2}."
-
-  2.6.1 :001 > meal_choice('carrots', 'string beans', 'lentils')
-What a nutritious meal!
-A plate of lentils with carrots and string beans.
- => "A plate of lentils with carrots and string beans."
+end
